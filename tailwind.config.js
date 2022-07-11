105a7dae1f07e6cd1c7ed0809cf3fe78160c0987
Khalteck <khalidoyeneye@gmail.com>
2022-07-11 07:41:39 -0600

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'kumbh': 'Kumbh Sans'
+      }
+    },
+  },
+  plugins: [],
+}
