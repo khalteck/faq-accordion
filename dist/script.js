17f98b3c6f664e8811e08e2f5982ed96a2e9a888
Khalteck <khalidoyeneye@gmail.com>
2022-07-12 11:19:35 -0600

    commit

--- dist/script.js
+++ dist/script.js
@@ -77,9 +77,28 @@ q5.addEventListener("click", () => {
     }
 })
 
-//the questions
-let quest1 = document.getElementById("quest1");
-let quest2 = document.getElementById("quest2");
-let quest3 = document.getElementById("quest3");
-let quest4 = document.getElementById("quest4");
-let quest5 = document.getElementById("quest5");
+/*let arr = [
+    document.getElementById("q1"), 
+    document.getElementById("q2"), 
+    document.getElementById("q3"), 
+    document.getElementById("q4"), 
+    document.getElementById("q5")
+];
+arr.values().addEventListener("click", function() {
+    let toggle = (ans, close, clicked) => {
+        if (ans.classList.contains("hide")) {
+            ans.classList.remove("hide");
+            close.style.transform = "rotate(180deg)";
+            clicked = true;
+        } else if (clicked === true) {
+            ans.classList.add("hide");
+            close.style.transform = "rotate(360deg)";
+            clicked = false;
+        }
+    }
+    toggle(ans1, close1, clicked1);
+    toggle(ans2, close2, clicked2);
+    toggle(ans3, close3, clicked3);
+    toggle(ans4, close4, clicked4);
+    toggle(ans5, close5, clicked5);
+})*/
